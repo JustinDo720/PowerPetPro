e12bb63fff9ab2e148d73ab0f04a348fcc220013
Your Name <justindo720@gmail.com>
2021-10-12 23:39:24 -0400

    Added UserCart functionality.

--- power_pet_pro_app/urls.py
+++ power_pet_pro_app/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('password/reset/confirm/<str:uid>/<str:token>/', views.reset_password, name='reset_password'),
     path('profile_list/user_profile/<int:user_id>/', views.UserProfile.as_view(), name='user_profile'),
     path('profile_list/user_profile/<int:user_id>/cart/', views.UserCart.as_view(), name='user_cart'),
+    path('profile_list/user_profile/<int:user_id>/cart/<int:product_id>/', views.updateUserCart, name='update_user_cart'),
 ]
--- power_pet_pro_app/views.py
+++ power_pet_pro_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from rest_framework.generics import ListCreateAPIView, ListAPIView
 from rest_framework.response import Response
-from .serializers import ProductSerializer, CategorySerializer, ProfileSerializer, CustomUserSerializer, CartItemSerializer
+from .serializers import ProductSerializer, CategorySerializer, ProfileSerializer, CustomUserSerializer, \
+    CartItemSerializer
 from .models import Product, Category, CartItem
 from rest_framework.views import APIView
 from rest_framework.pagination import PageNumberPagination
@@ -15,7 +16,10 @@ from users.models import Profile
 
 # Error Handling
 from django.http import Http404, HttpResponse
+from django.db import IntegrityError
 from rest_framework import status
+
+
 # Create your views here.
 
 
@@ -51,7 +55,7 @@ class CategoryList(APIView):
     """
 
     def get(self, request):
-        categories = Category.objects.all() # Note that these categories are already ordered by name
+        categories = Category.objects.all()  # Note that these categories are already ordered by name
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -124,13 +128,14 @@ class LatestProducts(APIView):
     """
     List the first 5 items that are in our Products model
     """
+
     def get(self, request):
-        latest_products = Product.objects.all()[:5]     # 5 items [start:stop] stop excludes similar to range
+        latest_products = Product.objects.all()[:5]  # 5 items [start:stop] stop excludes similar to range
         serializer = ProductSerializer(latest_products, many=True)
         return Response(serializer.data)
 
 
-@api_view(['POST']) # when making an api view you have to specify which methods you want so
+@api_view(['POST'])  # when making an api view you have to specify which methods you want so
 def search(request):
     query = request.data.get('query', '')
 
@@ -208,12 +213,52 @@ class UserCart(APIView):
         serializer = CartItemSerializer(cart, many=True)
         return Response(serializer.data)
 
-    # lets handle posting cart items to user cart
-    def post(self, request, user_id, format=None):
-        # since we are just posting a cart object we wont need the user_id here
-        serializer = CartItemSerializer(data=request.data)
-        # heres where we actually handle the post request
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # # lets handle posting cart items to user cart
+    # def post(self, request, user_id, format=None):
+    #     # since we are just posting a cart object we wont need the user_id here
+    #     serializer = CartItemSerializer(data=request.data)
+    #     # heres where we actually handle the post request
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #
+    # # lets handle posting cart items to user cart
+    # def put(self, request, user_id, format=None):
+    #     # since we are just posting a cart object we wont need the user_id here
+    #     serializer = CartItemSerializer(data=request.data)
+    #     # heres where we actually handle the post request
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST', 'PUT', 'DELETE'])  # when making a function based api view you have to specify which methods you want
+def updateUserCart(request, user_id, product_id):
+    try:
+        product = CartItem.objects.filter(profile=user_id).get(product=product_id)
+    except CartItem.DoesNotExist:
+        # if product does not exist but we are posting then we will pass the exception otherwise its a put or delete
+        if request.method == 'POST':
+            pass
+        else:
+            # if the method is put or delete then we need a product which means we will raise an Http404
+            raise Http404
+
+    if request.method == 'POST':
+        # For post we dont need our main serializer because we are not changing a specific product we are just adding
+        post_serializer = CartItemSerializer(data=request.data)
+        if post_serializer.is_valid():
+            post_serializer.save()
+            return Response(post_serializer.data)
+        return Resposne(post_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'PUT':
+        main_serializer = CartItemSerializer(prodpostuct, many=False, data=request.data)
+        if main_serializer.is_valid():
+            main_serializer.save()
+            return Response(main_serializer.data)
+        return Resposne(main_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response({"Success": "You have deleted an item from your cart."}, status=status.HTTP_400_BAD_REQUEST)
